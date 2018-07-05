73ce93915041f3f7269233cdb901ef0a64fb822c
JavaJimbo <JimSedgwick66@gmail.com>
2018-07-04 21:19:12 -0400

    Add files via upload
    Renamed project file to XYZservo

---
+++ XYZ Servo/XYZservo.X/build/default/production/_ext/1472/main.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1472/main.o:  \
+../main.c  \
+../HardwareProfile.h  \
+ ../HardwareProfile\ -\ PIC32MX795F512L\ PIM.h  \
+../Delay.h  \
+ ../GenericTypeDefs.h  \
+../Compiler.h 
---
+++ XYZ Servo/XYZservo.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XYZservo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XYZservo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xyzservo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xyzservo.x.tar
